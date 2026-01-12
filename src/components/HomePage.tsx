28792c6c167db5f31097c45eb7359b9ed3010415
solinkz <solomoneyitene@gmail.com>
2026-01-12 17:59:27 +0100

    Adjusted alert styling

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,8 @@
 import { HeaderSection } from "./formSection/HeaderSection";
 import { FormSelection } from "./formSection/FormSelection";
 import { CritiqueContainer } from "./critiqueSection/CritiqueContainer";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Info } from "lucide-react";
 export function HomePage() {
   return (
     <div className="mx-auto grid min-h-screen w-full max-w-6xl min-w-0 content-center items-start gap-8 p-4 pt-2 sm:gap-12 sm:p-6 md:grid-cols-5 md:gap-20 md:items-center lg:p-2">
@@ -9,7 +11,14 @@ export function HomePage() {
         <FormSelection />
       </div>
       {/* Critique section */}
-      <CritiqueContainer />
+      <div className="flex flex-col gap-4 col-span-3">
+        <CritiqueContainer />
+        
+        <Alert className="border-0 p-0 text-muted-foreground">
+          <Info strokeWidth={3} />
+          <AlertTitle>Content generated may be wrong </AlertTitle>
+        </Alert>
+      </div>
     </div>
   );
 }
