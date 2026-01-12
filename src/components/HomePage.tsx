0a3d5c9cab763b12063089d497c38b08ed51135c
solinkz <solomoneyitene@gmail.com>
2026-01-12 14:55:07 +0100

    Refactored components around

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,6 @@
 import { HeaderSection } from "./formSection/HeaderSection";
 import { FormSelection } from "./formSection/FormSelection";
-import uxcopystate from "@/assets/uxcopystate.svg";
+import { CritiqueContainer } from "./critiqueSection/CritiqueContainer";
 export function HomePage() {
   return (
     <div className="mx-auto grid min-h-screen w-full max-w-6xl min-w-0 content-center items-start gap-8 p-4 pt-2 sm:gap-12 sm:p-6 md:grid-cols-5 md:gap-20 md:items-center lg:p-2">
@@ -8,18 +8,8 @@ export function HomePage() {
         <HeaderSection />
         <FormSelection />
       </div>
-
       {/* Critique section */}
-      <div className="col-span-3 bg-gray-50 md:min-h-[757px] rounded-xl border border-gray-100 p-4">
-        <div className="flex flex-col md:min-h-[757px] items-center justify-center">
-          <div className="w-[129px] text-center mx-auto flex flex-col items-center">
-            <img src={uxcopystate} alt="empty state icon" />
-            <p className="text-gray-500 font-semibold leading-5 opacity-75">
-              Your critique will appear here
-            </p>
-          </div>
-        </div>
-      </div>
+      <CritiqueContainer />
     </div>
   );
 }
---
+++ src/components/critiqueSection/CritiqueContainer.tsx
@@ -0,0 +1,8 @@
+import { EmptyState } from "./emptyState";
+export function CritiqueContainer() {
+  return (
+    <div className="col-span-3 bg-gray-50 md:min-h-[757px] rounded-xl border border-gray-100 p-4">
+      <EmptyState />
+    </div>
+  );
+}
