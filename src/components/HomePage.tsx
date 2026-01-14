d4cab2ef82afc89aa13428f60ebf8a69c5083ae8
solinkz <solomoneyitene@gmail.com>
2026-01-14 18:40:02 +0100

    small fix for "/n"

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -20,7 +20,7 @@ export function HomePage() {
   return (
     <div className="mx-auto grid min-h-screen w-full max-w-6xl min-w-0 content-center items-start gap-6 p-3 pt-2 sm:gap-8 sm:p-4 md:grid-cols-5 md:gap-12 md:items-center lg:p-2">
       <div className="col-span-full md:col-span-2">
-        \n <HeaderSection />
+        <HeaderSection />
         <FormSelection
           uiCopy={uiCopy}
           setUiCopy={setUiCopy}
