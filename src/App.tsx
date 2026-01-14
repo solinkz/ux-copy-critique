fb8157021460419e4118d139362e7999501d1910
solinkz <solomoneyitene@gmail.com>
2026-01-14 20:30:12 +0100

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { HomePage } from "@/components/HomePage";
+import { Analytics } from "@vercel/analytics/react";
 
 export function App() {
-  return <HomePage />;
+  return (
+    <>
+      <HomePage />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
